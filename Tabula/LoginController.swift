d01e3a62e01038e7d635d606576c8568962fcdc7
adiveli <adi.veli@ymail.com>
2018-10-12 15:48:04 +0300

    Initial commit

---
+++ Tabula/LoginController.swift
@@ -0,0 +1,106 @@
+//
+//  LoginController.swift
+//  Tabula
+//
+//  Created by Adi Veliman on 11/10/2018.
+//  Copyright © 2018 Adi Veliman. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import GoogleSignIn
+
+class LoginController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate{
+    
+    
+    let URL_USER_LOGIN = "https://vaportofolio.ro/php/v1/login.php"
+    
+    //the defaultvalues to store user data
+    let defaultValues = UserDefaults.standard
+    
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var googleButton : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        googleButton.addTarget(self, action: #selector(signInUsingGoogle(_:)), for: .touchUpInside)
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /////////////////////GOOGLE SIGN IN////////////////////
+    
+    @objc func signInUsingGoogle(_ sender: UIButton){
+        GIDSignIn.sharedInstance()?.delegate = self
+        GIDSignIn.sharedInstance()?.uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error{
+            print("Error signing in: \(error.localizedDescription)")
+        }else{
+            if let gmailUser = user{
+                print("working!\(gmailUser.profile.email)")
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////////
+    
+    @IBAction func signInPressed(_ sender: Any) {
+        
+        //getting the username and password
+        let parameters: Parameters=[
+            "Email":emailField.text!,
+            "Password":passwordField.text!
+        ]
+        
+        //making a post request
+        Alamofire.request(URL_USER_LOGIN, method: .post, parameters: parameters).responseJSON
+            {
+                response in
+                //printing response
+                print(response)
+                
+                //getting the json value from the server
+                if let result = response.result.value {
+                    let jsonData = result as! NSDictionary
+                    
+                    //if there is no error
+                    if(!(jsonData.value(forKey: "error") as! Bool)){
+                        
+                        //getting the user from response
+                        let user = jsonData.value(forKey: "user") as! NSDictionary
+                        
+                        //getting user values
+                        let userId = user.value(forKey: "ID") as! Int
+                        let firstName = user.value(forKey: "FirstName") as! String
+                        let lastName = user.value(forKey: "LastName") as! String
+                        let userPhone = user.value(forKey: "TN") as! String
+                        
+                        //saving user values to defaults
+                        self.defaultValues.set(userId, forKey: "userid")
+                        self.defaultValues.set(firstName, forKey: "firstName")
+                        self.defaultValues.set(lastName, forKey: "lastName")
+                        self.defaultValues.set(userPhone, forKey: "userphone")
+                        
+                        //switching the screen
+                        
+                        print("It worked!")
+                        
+                    }else{
+                        //error message in case of invalid credential
+                        print("Invalid username or password")
+                    }
+                }
+        }
+        
+    }
+    
+
+}
---
+++ Tabula/RegistrationController.swift
@@ -0,0 +1,64 @@
+//
+//  RegistrationController.swift
+//  Tabula
+//
+//  Created by Adi Veliman on 11/10/2018.
+//  Copyright © 2018 Adi Veliman. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var cnpField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var telephoneField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    
+    //URL for web service
+    let URL_USER_REGISTER = "https://vaportofolio.ro/php/v1/register.php"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signInPressed(_ sender: Any) {
+        
+        
+        //creating parameters for the post request
+        let parameters: Parameters=[
+            "FirstName":firstNameField.text!,
+            "LastName":lastNameField.text!,
+            "Password":passwordField.text!,
+            "CNP":cnpField.text!,
+            "TN":telephoneField.text!,
+            "Email":emailField.text!,
+        ]
+        
+        //Sending http post request
+        Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters).responseJSON
+            {
+                response in
+                //printing response
+                print(response)
+                
+                //getting the json value from the server
+                if let result = response.result.value {
+                    
+                    //converting it as NSDictionary
+                    let jsonData = result as! NSDictionary
+                    
+                }
+        }
+        
+        
+    }
+    
+
+}
